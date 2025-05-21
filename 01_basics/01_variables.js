c69d736824bf93b6669d378fc067c1c0a7b1858d
Silu Nahak <nahaksilu2001@gmail.com>
2025-05-21 14:54:38 +0000

    variables type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+ accountCity = "Hyderabad"
+
+
+//accountId = 2//   not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "745803"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+
+console.table([accountId ,accountEmail ,accountPassword , accountCity])
