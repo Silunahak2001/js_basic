d0d655b22737bee10390bb514710a5491bb0e5fa
Silu Nahak <nahaksilu2001@gmail.com>
2025-05-21 15:43:26 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+// alert (3+3) // we are using nodejs, not browser
+
+
+console.log(3+3)
+// code readability should be high
+ console.log("Hitesh")
+
+ let name = "Hitesh"
+ let age = 18
+ let isLoggedIn = false
+
+ 
+ //  Primitive datatype//
+ // number => 2 to power of 53
+ //bigInt 
+ // String => ""
+ //boolean => true/false
+ // null => standalone value
+ // undefinied => value not assign
+ // symbol => unique
+
+ // object //
+
+ console.log(typeof undefinied);//undefined
+ console.log(typeof null);// object
+ 
