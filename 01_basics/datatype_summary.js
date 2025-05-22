fa777db0aa9f49e3cd3251c52486a656a25039c7
Silu Nahak <nahaksilu2001@gmail.com>
2025-05-22 03:49:46 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,33 @@
+//Primitive 
+
+//7 types: String , Number ,Boolean , nul ,undefined , symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+//const bigNumber =3456783456547545n
+
+
+
+//Reference type (Non- Primitive)
+
+
+// Array , objects , Functions
+
+const heros =  ["shaktiman" ,"nagraj" ,"doga"];
+let myObj ={
+    name: "virat",
+    age: 37,
+}
+const myFunction= function(){
+        console.log("Hello World");
+       
+}
+console.log(typeof bigNumber);
