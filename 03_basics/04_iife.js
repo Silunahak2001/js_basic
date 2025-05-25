a5131b49bbce9384f7b0583a043debf3cfa88039
Silu Nahak <nahaksilu2001@gmail.com>
2025-05-25 10:12:15 +0000

    IIFE.JS

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expression(IIFE)
+
+
+(function chai(){
+    //named IIFE
+    console.log(`DB connected`);
+    
+})();
+
+( ( name) =>{
+    console.log(`DB connected Two ${name}`);
+    
+})('hitesh')
+
+
+//javascript execution contest//
