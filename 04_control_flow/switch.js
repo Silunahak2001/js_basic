b10fca7bf5904f5a7611011203f8b53953ff0eae
Silu Nahak <nahaksilu2001@gmail.com>
2025-05-26 10:04:26 +0000

    switchjs

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+//switch//
+
+const month = "april"
+
+switch(month){
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "frb":
+        console.log("Feb");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        //break;
+    case "april":
+        console.log("april");
+        
+        break;
+    case "may":
+        console.log("may");
+        
+        break;
+        default:
+            console.log("default case match");
+            
+            break;
+}
+
+
+
